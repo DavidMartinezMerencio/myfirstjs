c423a88d833b8c064859ba7960f31ab780a80952
DavidMartinezMerencio <davidmartinez.20@campuscamara.es>
2022-02-02 14:11:59 +0100

    bind_close

--- js/form.js
+++ js/form.js
@@ -13,8 +13,27 @@ $("#btn").addEventListener("click", function() {
 });
 
 function myAlert(msg) {
+    console.log($("body").children[1]);
     var div = document.createElement("div");
     div.classList.add("alert");
     div.innerHTML = msg;
+    var close = document.createElement("span");
+    close.style.float = "right";
+    close.style.color = "red";
+    close.style.backgroundColor = "black";
+    close.classList.add("close");
+    close.innerHTML = "X";
+    div.appendChild(close);
     $("body").insertBefore(div, $("body").firstChild);
+    bind_close();
+}
+
+function bind_close() {
+    let elements = document.querySelectorAll(".close");
+    for (var i = elements.length-1; i >= 0; i--) {
+        let el = elements[i];
+        el.addEventListener("click", function() {
+            this.parentNode.style.display = "none";
+        });
+    }
 }
